6029dad0175b95cfffc2d94efb0d98485127789d
Andrzej <andrzuk@wp.pl>
2021-02-02 13:17:48 +0100

    Add files via upload

--- config/config.php
+++ config/config.php
@@ -9,4 +9,11 @@ define ('DB_NAME', '');  // db name
 define ('DB_USER', '');  // db username
 define ('DB_PASS', '');  // db password
 
+/*
+ * Mail account params
+ */
+ 
+define ('MAIL_SENDER_NAME', 'Mail Sender');  // email sender name
+define ('MAIL_SENDER_ADDRESS', 'sender@domain.com');  // email sender address
+
 ?>
